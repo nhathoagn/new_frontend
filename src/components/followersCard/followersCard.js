6bcce392cd5a3770e1424dc5dd8f8964a4f5a71e
nhathoagn <nguyennhathoang35@gmail.com>
2022-12-13 15:33:13 +0700

    feat: followers

---
+++ src/components/followersCard/followersCard.js
@@ -0,0 +1,38 @@
+import {useEffect, useState} from "react";
+import {useSelector} from "react-redux";
+
+const FollowersCard = ({ location }) => {
+    const [modalOpened, setModalOpened] = useState(false);
+    const [persons, setPersons] = useState([]);
+    const { user } = useSelector((state) => state.authReducer.authData);
+
+    useEffect(() => {
+        const fetchPersons = async () => {
+            const { data } = await getAllUser();
+            setPersons(data);
+        };
+        fetchPersons();
+    }, []);
+
+    return (
+        <div className="FollowersCard">
+            <h3>People you may know</h3>
+
+            {persons.map((person, id) => {
+                if (person._id !== user._id) return <User person={person} key={id} />;
+            })}
+            {!location ? (
+                <span onClick={() => setModalOpened(true)}>Show more</span>
+            ) : (
+                ""
+            )}
+
+            <FollowersModal
+                modalOpened={modalOpened}
+                setModalOpened={setModalOpened}
+            />
+        </div>
+    );
+};
+
+export default FollowersCard;
--- src/components/profileside/profile.js
+++ src/components/profileside/profile.js
@@ -1,10 +1,13 @@
 import "./profile.css";
 import LogoSearch from "../logoSearch/logoSearch";
+import ProfileCard from "../ProfileCard/profileCard";
 const ProfileSide = () => {
-    return (
-        <div className="ProfileSide">
-            <LogoSearch/>
-        </div>
-    )
+  return (
+    <div className="ProfileSide">
+      <LogoSearch />
+        <ProfileCard location='homepage'/>
+
+    </div>
+  );
 };
 export default ProfileSide;
