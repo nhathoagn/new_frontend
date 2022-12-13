7be1d25c2179ff8af81dfac64f08e8ac879a1415
nhathoagn <nguyennhathoang35@gmail.com>
2022-12-13 12:45:19 +0700

    first commit

---
+++ src/components/profileside/profile.js
@@ -0,0 +1,10 @@
+import "./profile.css";
+import LogoSearch from "../logoSearch/logoSearch";
+const ProfileSide = () => {
+    return (
+        <div className="ProfileSide">
+            <LogoSearch/>
+        </div>
+    )
+};
+export default ProfileSide;
---
+++ src/page/home/home.js
@@ -0,0 +1,8 @@
+import "./home.css";
+import ProfileSide from "../../components/profileside/profile";
+const Home = () => {
+  return <div className="Home">
+    <ProfileSide/>
+  </div>;
+};
+export default Home;
